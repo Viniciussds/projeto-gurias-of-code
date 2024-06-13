5fd4d62c03493528843b68d6b7c459b4d5361208
vinisdssds <viniciussds2.aluno@unipampa.edu.br>
2024-06-13 12:58:15 -0300

    ADD infraestrutura django,docker-compose,dockerfile

---
+++ scripts/commands.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+# Run the command passed by the user
+
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
